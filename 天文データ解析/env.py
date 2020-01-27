16d9f70cacbf15a192cf5ed7bed105337cdac347
hashimoto naoki <hashimoto0106@gmail.com>
2020-01-27 22:53:49 +0900

    add

---
+++ 天文データ解析/env.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 27 21:23:22 2020
+
+@author: PC
+"""
+
+from pyraf import iraf
+from iraf import images
+from iraf import tv
+import pyfits
+from ds9 import ds9
