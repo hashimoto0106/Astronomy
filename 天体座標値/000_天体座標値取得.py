99d84df6760e921a01fa6f0cc69c0ef754dad900
hashimoto naoki <hashimoto0106@gmail.com>
2020-05-13 02:21:11 +0900

    add

--- 天体座標値/000_天体座標値取得.py
+++ 天体座標値/000_天体座標値取得.py
@@ -5,13 +5,11 @@ Created on Sun May  3 15:00:20 2020
 @author: PC
 """
 
-from astropy import units as u
-from astropy.coordinates import SkyCoord, EarthLocation, AltAz
-from astropy.time import Time
+from astropy.coordinates import SkyCoord
 
 
 # 天体座標値取得(ICRS座標系)
-m33 = SkyCoord.from_name('M33') 
+m33 = SkyCoord.from_name('M33')
 print(m33)
 print("ra : ", m33.ra)
 print("dec : ", m33.dec)
--- 天体座標値/001_ICRS→AltAz座標変換.py
+++ 天体座標値/001_ICRS→AltAz座標変換.py
@@ -11,7 +11,7 @@ from astropy.time import Time
 
 
 # 天体座標値取得(ICRS座標系)
-m33 = SkyCoord.from_name('M33') 
+m33 = SkyCoord.from_name('M33')
 print(m33)
 print("ra : ", m33.ra)
 print("dec : ", m33.dec)
@@ -22,7 +22,7 @@ time = Time('2012-7-12 23:00:00') - utcoffset
 
 # ICRS->AltAz座標変換
 bear_mountain = EarthLocation(lat=41.3*u.deg, lon=-74*u.deg, height=390*u.m)
-m33altaz = m33.transform_to(AltAz(obstime=time,location=bear_mountain))
+m33altaz = m33.transform_to(AltAz(obstime=time, location=bear_mountain))
 print(m33altaz)
 print("az : ", m33altaz.az)
 print("alt : ", m33altaz.alt)
